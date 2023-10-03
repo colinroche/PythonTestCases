21ad0f1a011fbf3a9370a6785c89581433e93928
colinroche <colinr@DESKTOP-464BVEN.localdomain>
2023-10-03 17:48:25 +0100

    Reverse a string

---
+++ ReverseString/reverse_string.py
@@ -0,0 +1,4 @@
+s = "StringReverse"  # initial string
+stringLength = len(s)  # calculate length of the list
+slicedString = s[stringLength::-1]  # slicing
+print(slicedString)  # print the reversed string
