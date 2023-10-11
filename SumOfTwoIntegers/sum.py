83841086b49787e02442e42d1c5e3d5fc1381726
colinroche <colinr@DESKTOP-464BVEN.localdomain>
2023-10-11 14:19:08 +0100

    Sum of 2 integers

---
+++ SumOfTwoIntegers/sum.py
@@ -0,0 +1,9 @@
+int_1 = input("Enter first integer: ")
+int_2 = input("Enter second integer: ")
+
+int_1 = int(int_1)
+int_2 = int(int_2)
+
+int_sum = int_1 + int_2
+
+print("Sum of integers: ", int_sum)
